4527640ea74e45c677f762a822a8203b77235992
Camilo <cm243@njit.edu>
2016-04-24 22:38:39 -0400

    Exams edited

--- includes/Requests/Exams.php
+++ includes/Requests/Exams.php
@@ -5,26 +5,33 @@ class Includes_Requests_Exams extends Includes_Requests_Abstract
 	{
 		parent::__construct($options);
 	}
-	
+
 	public function process($options=array())
 	{
-	
+
 	}
-	
+
 	public function getExams()
 	{
 		$url = ENDPOINT . '/get_exams';
 		$response = parent::request($url, '','GET', null);
 		return $response;
 	}
-	
+
 	public function getExamsByProfessorId($id)
 	{
 		$url = ENDPOINT . '/get_exams_by_professor_id/' . $id;
 		$response = parent::request($url, '','GET', null);
 		return $response;
 	}
-	
+
+	public function getExamById($id)
+	{
+		$url = ENDPOINT . '/get_exam_by_id/'. $id;
+		$response = parent::request($url, '','GET', null);
+		return $response;
+	}
+
 	public function addExam($data)
 	{
 		$url = ENDPOINT . '/add_exam';
